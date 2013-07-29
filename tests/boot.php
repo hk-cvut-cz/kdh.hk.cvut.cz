Mikulas/Skeleton21

added tests skeleton

tests/boot.php
@@ -1,0 +1,28 @@
+<?php
+
+namespace Tests;
+
+use Nette;
+use Nette\Diagnostics\Debugger;
+use Orm;
+
+define('TESTS_DIR', __DIR__);
+define('APP_DIR', __DIR__ . '/../app');
+define('LIBS_DIR', __DIR__ . '/../libs');
+define('TEMP_DIR', __DIR__ . '/../temp');
+
+require_once LIBS_DIR . '/autoload.php';
+
+
+Debugger::$strictMode = TRUE;
+Debugger::enable();
+
+$loader = new Nette\Loaders\RobotLoader;
+$loader->setCacheStorage(new Nette\Caching\Storages\FileStorage(TEMP_DIR . '/cache'));
+$loader->addDirectory(array(APP_DIR, TESTS_DIR . '/cases', TESTS_DIR . '/inc', LIBS_DIR . '/clevispace'));
+$loader->register();
+
+$configurator = new TestsConfigurator;
+$diContainer = $configurator->createContainer();
+
+Orm\PerformanceHelper::$keyCallback = NULL;

tests/test-www/index.php
@@ -1,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../../libs/autoload.php';
+require_once __DIR__ . '/../../app/config/Configurator.php';
+require_once __DIR__ . '/../inc/TestsRootConfigurator.php';
+
+$configurator = new Tests\TestsRootConfigurator(isset($_GET['testDbName']) ? $_GET['testDbName'] : NULL);
+
+$configurator->enableDebugger();
+$configurator
+	->createRobotLoader()
+	->addDirectory(__DIR__ . '/../inc')
+	->register()
+;
+
+$context = $configurator->createContainer();
+$context->application->run();

hk-cvut-cz/kdh.hk.cvut.cz

tests/boot.php
@@ -1,0 +1,28 @@
+<?php
+
+namespace Tests;
+
+use Nette;
+use Nette\Diagnostics\Debugger;
+use Orm;
+
+define('TESTS_DIR', __DIR__);
+define('APP_DIR', __DIR__ . '/../app');
+define('LIBS_DIR', __DIR__ . '/../vendor');
+define('TEMP_DIR', __DIR__ . '/../temp');
+
+require_once LIBS_DIR . '/autoload.php';
+
+
+Debugger::$strictMode = TRUE;
+Debugger::enable();
+
+$loader = new Nette\Loaders\RobotLoader;
+$loader->setCacheStorage(new Nette\Caching\Storages\FileStorage(TEMP_DIR . '/cache'));
+$loader->addDirectory(array(APP_DIR, TESTS_DIR . '/cases', TESTS_DIR . '/inc', LIBS_DIR . '/clevispace'));
+$loader->register();
+
+$configurator = new TestsConfigurator;
+$diContainer = $configurator->createContainer();
+
+Orm\PerformanceHelper::$keyCallback = NULL;

tests/test-www/index.php
@@ -1,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../app/config/Configurator.php';
+require_once __DIR__ . '/../inc/TestsRootConfigurator.php';
+
+$configurator = new Tests\TestsRootConfigurator(isset($_GET['testDbName']) ? $_GET['testDbName'] : NULL);
+
+$configurator->enableDebugger();
+$configurator
+	->createRobotLoader()
+	->addDirectory(__DIR__ . '/../inc')
+	->register()
+;
+
+$context = $configurator->createContainer();
+$context->application->run();