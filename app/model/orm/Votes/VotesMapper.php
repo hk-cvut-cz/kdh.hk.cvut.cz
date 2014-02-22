1194beb2db6b0ee15e5cdf643382bcfeebbae58e
Mikulas <rullaf@gmail.com>
2014-02-22 12:42:36 +0100

    fix vote count

--- app/model/orm/Votes/VotesMapper.php
+++ app/model/orm/Votes/VotesMapper.php
@@ -9,4 +9,13 @@ use Orm\IRepository;
 class VotesMapper extends Mapper
 {
 
+	public function findAll()
+	{
+		return $this->dataSource('
+			SELECT [r].* FROM %n [r]', $this->getTableName(), '
+			LEFT JOIN [games] [g] ON [r.game_id]=[g.id]
+			WHERE [g.status] = %s', Game::PROPOSED
+		);
+	}
+
 }
